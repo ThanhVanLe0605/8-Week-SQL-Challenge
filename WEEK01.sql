bb5b849bfab82a9b710c0e60d2c4620e0a92e21c
Lê Thanh Vạn <odon29032005@gmail.com>
2026-04-23 14:12:54 +0700

    Add files via upload
    git commit -m "feat: hoàn thành 6 bài đầu tiên"

--- WEEK01.sql
+++ WEEK01.sql
@@ -1,6 +1,9 @@
 ﻿create database sql_challenge
 use sql_challenge
 
+-- LINK 01 : https://www.youtube.com/watch?v=cXhv4kmIzFw
+-- LINK 02: https://youtu.be/OdnxoJitdAg?si=Mnyt5e6ViZYPI9K5
+-- LINK 03: 
 --  week 01: Case Study #1 - Danny's Diner Danny 
 
 -- 1. TẠO SCHEMA
@@ -173,38 +176,228 @@ FROM dannys_diner.sales s
 INNER JOIN dannys_diner.members m ON s.customer_id = m.customer_id
 INNER JOIN dannys_diner.menu n ON s.product_id = n.product_id
 
+-- CHÚ Ý THỰC TIỄN:
+-- Khách có thể mua sắm nhiều lần cùng một ngày
+-- Nên nên để DATETIME thay vì DATE 
+-- Thay vì dùng tổ hợp làm khóa chính , thì tham khảo surrogate key, natural key , auto increment ID 
+
+
 ------------------------------------------------
 -- 5. Case Study Questions
 ------------------------------------------------
--- 5.1. What is the total amount each customer spent at the restaurant?
-------------------------------------------------
--- 5.2. How many days has each customer visited the restaurant?
-------------------------------------------------
--- 5.3. What was the first item from the menu purchased by each customer?
-------------------------------------------------
--- 5.4. What is the most purchased item on the menu and how many times was it purchased by all customers?
-------------------------------------------------
--- 5.5. Which item was the most popular for each customer?
-------------------------------------------------
--- 5.6. Which item was purchased first by the customer after they became a member?
-------------------------------------------------
--- 5.7. Which item was purchased just before the customer became a member?
-------------------------------------------------
--- 5.8. What is the total items and amount spent for each member before they became a member?
-------------------------------------------------
--- 5.9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
-------------------------------------------------
--- 5.10. 
--- In the first week after a customer joins the program (including their join date) 
--- they earn 2x points on all items, not just sushi
--- how many points do customer A and B have at the end of January?
+-- 5.1. Tổng số tiền mỗi khách hàng đã chi tiêu tại nhà hàng là bao nhiêu?
+
+-- CÁCH 1 cho bài 5.1:
+-- Bước 1: Tạo CTE với output customer_id, order_date, total_amount_by_orderdate
+-- để ghép bảng lấy giá 
+WITH sales_with_price  AS (
+	SELECT 
+			s.customer_id,
+			s.order_date,
+			m.product_id,
+			m.price
+	FROM dannys_diner.sales s
+	INNER JOIN dannys_diner.menu m ON s.product_id = m.product_id
+	
+	) 
+-- Bước 2: Viết query từ bảng CTE đã tạo 
+SELECT 
+		sp.customer_id,
+		SUM(sp.price) AS total_amount_by_customers
+FROM sales_with_price sp
+GROUP BY sp.customer_id
+ORDER BY total_amount_by_customers ASC
+
+-- CÁCH 2 cho bài 5.1:
+SELECT
+		s.customer_id,
+		SUM(m.price) AS total_amount_by_customers
+FROM dannys_diner.sales s
+INNER JOIN dannys_diner.menu m ON s.product_id = m.product_id
+GROUP BY s.customer_id
+ORDER BY total_amount_by_customers DESC 
 
 ------------------------------------------------
--- 6. Bonus Questions
+-- 5.2. Mỗi khách hàng đã ghé thăm nhà hàng bao nhiêu ngày?
+
+-- Hiểu logic nghiệp vụ của dữ liệu 
+-- order_date bảng sales là ngày khách hàng đến thăm , ngày đến để mua hàng
+-- join_date ở members là ngày đăng ký thẻ thành viên
+
+-- Xem dữ liệu 
+SELECT *
+FROM dannys_diner.sales s
+-- A	2021-01-01	1
+-- A	2021-01-01	2
+-- Nhận xét: vào cùng một ngày, mua sp có product_id =1 và 2
+-- việc hiển thị chi tiết pro_id trong đơn hàng làm order_date bị lặp lại nhiều lần
+-- nên khi đếm ngày khách hàng đến mua hàng phải dùng COUNT(DISTINCT .. )
+
+-- Viết câu lệnh truy xuất thông tin
+SELECT
+		s.customer_id,
+		COUNT(DISTINCT s.order_date) AS visited_days
+FROM dannys_diner.sales s
+GROUP BY s.customer_id
+ORDER BY visited_days DESC
+
 ------------------------------------------------
--- 6.1. Join All The Things
+-- 5.3. Món đầu tiên trong thực đơn mà mỗi khách hàng đã mua là gì?
+
+-- Một khách hàng sẽ có nhiều ngày mua hàng, một ngày mua hàng có thể mua nhiều sản phẩm
+-- Khi tính tiền, thời gian theo lúc tạo hóa đơn chứ không theo lúc quét mã vạch từng sản phẩm 
+
+-- Vậy sản phầm đầu tiên sẽ là ?  
+-- Tất cả sản phẩm trong giỏ hàng của ngày đầu tiên khách hàng mua
+
+-- Để giải bài toán này
+-- Không thể dùng TOP 1 vì nó chỉ dành cho 1 cột
+-- groupby theo nhiều cấp, lấy thứ hạng của cấp 2, 3,... --> ?
+-- Dùng Rank Functions
+-- Dùng DENSE_RANK để giải quyết vì: nó cho phép đồng hạng, phù hợp để lấy ra ngày mua hàng sớm nhất của khách hàng 
+
+-- Bước 1: Dùng CTE để xếp hạng ngày mua hàng của từng khách hàng 
+;WITH ranked_sales AS (
+		SELECT
+				s.customer_id,
+				s.order_date,
+				
+				-- Hàm DENSE_RANK():
+				-- PARTITION BY: Chia nhóm theo từng khách hàng
+				-- ORDER BY: Sắp xếp ngày mua tăng dần (cũ nhất lên đầu)
+				DENSE_RANK() OVER (PARTITION BY s.customer_id ORDER BY s.order_date ASC) AS [rank],
+
+				m.product_name 
+		FROM dannys_diner.sales s 
+		INNER JOIN dannys_diner.menu m ON s.product_id = m.product_id 
+		GROUP BY s.customer_id,
+				 s.order_date,
+				 m.product_name 
+)		
+-- Bước 2: Lọc ra đơn hàng có ngày đầu tiên
+SELECT *
+FROM ranked_sales r
+WHERE r.[rank] = 1
+
+
 ------------------------------------------------
--- 6.2. Rank All The Things
+-- 5.4. Món được mua nhiều nhất trong thực đơn là gì và nó đã được tất cả khách hàng mua bao nhiêu lần?
+-- CÁCH 1 cho 5.4:
+;WITH sales_table  AS(
+		SELECT m.product_id,
+			   m.product_name,
+			   COUNT(s.order_date) AS saled_amt,
+			   DENSE_RANK() OVER(ORDER BY COUNT(s.order_date) DESC) AS rank_saled_amnt
+		FROM dannys_diner.sales s 
+		INNER JOIN dannys_diner.menu m ON s.product_id = m.product_id
+		GROUP BY m.product_id,
+				 m.product_name
+		
+)
+SELECT  
+	  s.product_id AS best_sell_product_id,
+	  s.product_name AS best_sell_product_name,
+	  s.saled_amt AS saled_amnt
+FROM  sales_table s
+WHERE s.rank_saled_amnt = 1
+
+-- CÁCH 2 cho 5.4:
+SELECT TOP 1 WITH TIES
+    m.product_id AS best_sell_product_id,
+    m.product_name AS best_sell_product_name,
+    COUNT(1) AS saled_amnt
+FROM dannys_diner.sales s
+INNER JOIN dannys_diner.menu m ON s.product_id = m.product_id
+GROUP BY 
+    m.product_id,
+    m.product_name
+ORDER BY saled_amnt DESC;
+
+
+------------------------------------------------
+-- 5.5. Món nào phổ biến nhất đối với mỗi khách hàng?
+
+-- nhóm cấp 1: khách hàng
+-- nhóm cấp 2: product_id 
+WITH rank_purchae_times AS(
+	SELECT 
+			s.customer_id,
+			m.product_id,
+			m.product_name,
+			COUNT(s.order_date) AS times,
+			DENSE_RANK() OVER(PARTITION BY s.customer_id ORDER BY COUNT(s.order_date) DESC ) AS rank_purchase_times
+	FROM dannys_diner.sales s
+	INNER JOIN dannys_diner.menu m ON s.product_id = m.product_id
+	GROUP BY    s.customer_id,
+				m.product_id,
+				m.product_name
+	
+)
+
+SELECT
+		r.customer_id,
+		r.product_name	AS best_seller,
+		r.times
+FROM	rank_purchae_times r
+WHERE r.rank_purchase_times = 1
+ORDER BY   r.customer_id ASC
+
+
+------------------------------------------------
+-- 5.6. Món nào được khách hàng mua đầu tiên sau khi họ trở thành thành viên?
+
+-- khách hàng có thể là khách tại quán, sau một thời gian sử dụng thì đk thẻ thành viên
+-- vậy ngày mua hàng sau khi đăng ký thẻ chưa chắc là ngày mua hàng sớm nhất trong cột order_date bảng sales 
+WITH rank_date_by_cust  AS(
+	SELECT 
+			s.customer_id,
+			m.product_id,
+			m.product_name,
+			s.order_date,
+			b.join_date,
+			DENSE_RANK() OVER(PARTITION BY  s.customer_id ORDER BY s.order_date ASC) AS rank_date 
+	FROM dannys_diner.sales s
+	INNER JOIN dannys_diner.menu m ON s.product_id = m.product_id
+	INNER JOIN dannys_diner.members b ON s.customer_id = b.customer_id
+	WHERE  b.join_date <= s.order_date
+	GROUP BY    s.customer_id,
+				m.product_id,
+				m.product_name,
+				s.order_date,
+				b.join_date
+)
+SELECT
+		rd.customer_id,
+		rd.product_name AS first_purchased_items_by_every_cust,
+		rd.order_date,
+		rd.join_date
+FROM	rank_date_by_cust rd
+WHERE	rd.rank_date = 1
+
+-- dùng WHERE hay HAVING
+-- sau khi JOIN, dùng where để lọc record nào không thỏa đk 
+-- HAVING cũng để lọc, nhưng kết hợp với các hàm tổng hợp nhưu COUNT, SUM, AVG
+-- để đảm bảo hiệu suất, như trường hợp này, dùng WHERE chứ không dùng HAVING 
+
+
+------------------------------------------------
+-- 5.7. Món nào được mua ngay trước khi khách hàng trở thành thành viên?
+------------------------------------------------
+-- 5.8. Tổng số món và số tiền đã chi tiêu của mỗi thành viên trước khi họ trở thành thành viên là bao nhiêu?
+------------------------------------------------
+-- 5.9. Nếu mỗi 1 đô la chi tiêu tương đương 10 điểm và sushi có hệ số nhân điểm gấp đôi - thì mỗi khách hàng sẽ có bao nhiêu điểm?
+------------------------------------------------
+-- 5.10. 
+-- Trong tuần đầu tiên sau khi khách hàng tham gia chương trình (bao gồm ngày tham gia)
+-- họ kiếm được điểm gấp đôi cho tất cả các món, không chỉ sushi
+-- vậy vào cuối tháng 1, khách hàng A và B có bao nhiêu điểm?
+
+------------------------------------------------
+-- 6. Câu hỏi thưởng
+------------------------------------------------
+-- 6.1. Kết hợp tất cả các dữ liệu
+------------------------------------------------
+-- 6.2. Xếp hạng tất cả các dữ liệu
 ------------------------------------------------
 ------------------------------------------------
 ------------------------------------------------
